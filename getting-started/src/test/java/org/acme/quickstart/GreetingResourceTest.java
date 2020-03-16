060ba4c2072df7dde17f2d5b5c0fc7a344213e48
nelson <srandrediaz@gmail.com>
2020-03-16 22:07:09 +0100

    added testing methods and generate the package

--- getting-started/src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ getting-started/src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -1,21 +1,44 @@
 package org.acme.quickstart;
 
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import java.net.URL;
+import java.util.UUID;
+
+//@QuarkusTest - instruct JUnit to start the application before the tests
 @QuarkusTest
 public class GreetingResourceTest {
 
+	/*testing both rest endpoints*/
+	
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/C:/Program Files (x86)/Git/hello")
+          .when().get("hello")
           .then()
              .statusCode(200)
              .body(is("hello"));
     }
+    
+    @Test
+    public void testGreetingEndpoint() {
+    	
+    	//generate a random name
+    	String uuid = UUID.randomUUID().toString();
+    	
+    	given()
+    	.pathParam("name", uuid)
+    	.then()
+    		.statusCode(200)
+    		.body(is("hello "+ uuid));
+    	
+    }
+    
+  
 
 }
