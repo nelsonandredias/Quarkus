d563813e2867c3cdcca527ab2cebcee4c0ef9c2f
nelson <srandrediaz@gmail.com>
2020-03-28 21:32:58 +0100

    log active profile onstartup

--- getting-started/src/main/java/org/acme/quickstart/application/ApplicationLifeCycle.java
+++ getting-started/src/main/java/org/acme/quickstart/application/ApplicationLifeCycle.java
@@ -7,6 +7,7 @@ import org.jboss.logging.Logger;
 
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
+import io.quarkus.runtime.configuration.ProfileManager;
 
 @ApplicationScoped
 public class ApplicationLifeCycle {
@@ -14,13 +15,18 @@ public class ApplicationLifeCycle {
 	private static final Logger LOGGER = Logger.getLogger(ApplicationLifeCycle.class);
 	
 	void onStartup(@Observes StartupEvent event) {
+		
+		// log startup banner
 		LOGGER.info("  ___ ___         .__  .__          ");
         LOGGER.info(" /   |   \\   ____ |  | |  |   ____  ");
         LOGGER.info("/    ~    \\_/ __ \\|  | |  |  /  _ \\ ");
         LOGGER.info("\\    Y    /\\  ___/|  |_|  |_(  <_> )");
         LOGGER.info(" \\___|_  /  \\___  >____/____/\\____/ ");
         LOGGER.info("       \\/       \\/                  ");
-        LOGGER.info("                         Powered by Quarkus");             
+        LOGGER.info("                         Powered by Quarkus");  
+        
+        // log activated profile 
+        LOGGER.infof("The application is starting with profile `%s`", ProfileManager.getActiveProfile());
 	}
 	
 	void onShutdown(@Observes ShutdownEvent event) {
